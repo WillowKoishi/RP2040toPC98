dc8aa72a423a52b5967c655aa9bb3b20be0fdb25
柳树怪 <wolliw@163.com>
2022-06-06 11:01:22 +0800

    succeceful connect to PC98

--- main.c
+++ main.c
@@ -1,45 +1,33 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 
-/// \tag::hello_uart[]
-
 #define UART_ID uart1
-#define BAUD_RATE 19200
 
-// We are using pins 0 and 1, but see the GPIO function select table in the
-// datasheet for information on which other pins can be used.
 #define UART_TX_PIN 4
-#define UART_RX_PIN 1
-
+void send98(int msg);
 int main() {
-    // Set up our UART with the required speed.
+    gpio_init(2);
+    gpio_set_dir(2, GPIO_IN);
+    gpio_init(3);
+    gpio_set_dir(3, GPIO_IN);
     uart_init(UART_ID, 19200);
-
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);//设置TX
-
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    char data = 0x1c;
-    // Send out a character without any conversions
-    uart_putc_raw(UART_ID, data);
+    uart_set_format(UART_ID,8,2,UART_PARITY_ODD);
+    sleep_ms(1000);
+    send98(0x14);
+    sleep_ms(100);
+    send98(0x14 | 0x80);
     
-    // Send out a character but do CR/LF conversions
-    uart_putc(UART_ID, 'B');
-
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
 }
+//GPIO: RTY 2  RDY 3  RXD 4
 void send98(int msg){
-    
+    while(gpio_get(2));
+    char data = msg;
+    uart_putc_raw(UART_ID,data);
+    while(!gpio_get(2));
+    sleep_us(20);
+    if(!gpio_get(3)){
+        send98(msg);
+    }
 }
-/// \end::hello_uart[]
